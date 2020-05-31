86d73081563d6bae49852031885d6418e476cd74
varun <varungupta2015135@gmail.com>
2020-05-31 19:17:52 +0530

    Added test ping handler

--- main.go
+++ main.go
@@ -1,9 +1,36 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
 )
 
+type data struct {
+	Data interface{} `json:"data"`
+}
+
 func main() {
-	fmt.Println("Hello World!")
+	router := gin.Default()
+
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "OPTIONS"},
+		AllowHeaders:     []string{"*"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
+
+	router.GET("/ping", pingHandler)
+
+	router.Run()
+}
+
+func pingHandler(c *gin.Context) {
+	var data data
+	data.Data = "pong"
+	c.JSON(http.StatusOK, data)
 }
