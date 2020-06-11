b88a4af685440899b408937db854bf1a69fdab7a
Gupta <Varun.Gupta3@honeywell.com>
2020-06-11 19:11:40 +0530

    Removed old nav

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,10 @@
 import React from "react";
 import "./App.css";
-// import Nav from "./components/Nav/Nav";
 import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
-      {/* <Nav /> */}
       <Header />
     </div>
   );
