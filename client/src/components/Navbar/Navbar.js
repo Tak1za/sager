e8e67a8cbac6629558d351c9fa985aa89514287e
Gupta <Varun.Gupta3@honeywell.com>
2020-06-13 01:05:15 +0530

    Increased size of logo

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -34,7 +34,9 @@ function Navbar(props) {
           <Nav.Link
             as={Link}
             to="/"
-            className={props.location.pathname === "/" ? "active" : null}
+            className={
+              "logo" + (props.location.pathname === "/" ? " active" : "")
+            }
           >
             Sager
           </Nav.Link>
