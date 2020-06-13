dc02a4d33f587a1817ccc196a43882e0508da651
Gupta <Varun.Gupta3@honeywell.com>
2020-06-13 18:12:55 +0530

    Added sample data to render in list component

--- client/src/components/List/List.js
+++ client/src/components/List/List.js
@@ -1,30 +1,23 @@
-import React from 'react';
-import './List.scss';
+import React from "react";
+import "./List.scss";
+import { Container, Row } from "react-bootstrap";
 
-function List(){
-  return(
+function List() {
+  let data = Array(20).fill("lorem ipsum dolor");
+  console.log(data);
+  return (
     <div className="list">
-      <p>bfkdjfndjkfndskfnds</p>
-      <p>bfkdjfndjkfndskfnds</p>
-      <p>bfkdjfndjkfndskfnds</p>
-      <p>bfkdjfndjkfndskfnds</p>
-      <p>bfkdjfndjkfndskfnds</p>
-      <p>bfkdjfndjkfndskfnds</p>
-      <p>bfkdjfndjkfndskfnds</p>
-      <p>bfkdjfndjkfndskfnds</p>
-      <p>bfkdjfndjkfndskfnds</p>
-      <p>bfkdjfndjkfndskfnds</p><p>bfkdjfndjkfndskfnds</p>
-      <p>bfkdjfndjkfndskfnds</p>
-      <p>bfkdjfndjkfndskfnds</p>
-      <p>bfkdjfndjkfndskfnds</p>
-      <p>bfkdjfndjkfndskfnds</p>
-      <p>bfkdjfndjkfndskfnds</p>
-      <p>bfkdjfndjkfndskfnds</p>
-      <p>bfkdjfndjkfndskfnds</p>
-      <p>bfkdjfndjkfndskfnds</p>
-      <p>bfkdjfndjkfndskfnds</p>
+      <Container fluid>
+        {data.map((item, index) => {
+          return (
+            <Row key={index}>
+              <div className="list-item">{item}</div>
+            </Row>
+          );
+        })}
+      </Container>
     </div>
-  )
+  );
 }
 
 export default List;
