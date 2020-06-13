2fcc8de00f46286472b91f1b4673da72140efbd6
Gupta <Varun.Gupta3@honeywell.com>
2020-06-13 21:11:09 +0530

    Added playlist items rendering

--- client/src/components/List/List.js
+++ client/src/components/List/List.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./List.scss";
-import { Container, Row } from "react-bootstrap";
+import { Container, Media } from "react-bootstrap";
 
 function List() {
   let data = Array(20).fill("lorem ipsum dolor");
@@ -10,9 +10,22 @@ function List() {
       <Container fluid>
         {data.map((item, index) => {
           return (
-            <Row key={index}>
-              <div className="list-item">{item}</div>
-            </Row>
+            <Media key={index} className="list-item">
+              <img
+                width={64}
+                height={64}
+                className="mr-3"
+                src="https://46.media.tumblr.com/276eeec6ab6ee2dce7373580e5ffa35c/tumblr_n2fx9yLS411tvfpg9o1_500.gif"
+                alt="Generic placeholder"
+              />
+              <Media.Body>
+                <h6>{item}</h6>
+                <p>Varun Gupta</p>
+              </Media.Body>
+              <Media.Body className="d-flex flex-row-reverse">
+                <p>13</p>
+              </Media.Body>
+            </Media>
           );
         })}
       </Container>
