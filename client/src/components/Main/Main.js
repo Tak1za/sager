ef8239d2dadb227f63cc939284e64511c65fc7bd
Gupta <Varun.Gupta3@honeywell.com>
2020-06-15 00:02:31 +0530

    Added default breadcrumbs

--- client/src/components/Main/Main.js
+++ client/src/components/Main/Main.js
@@ -7,11 +7,16 @@ import Playlists from "../Playlists/Playlists";
 import Podcasts from "../Podcasts/Podcasts";
 import Explore from "../Explore/Explore";
 import Login from "../Login/Login";
+import { Breadcrumb } from "react-bootstrap";
 
 function Main(props) {
   return (
     <div className="main-container">
       {props.children}
+      <Breadcrumb style={{ backgroundColor: "white" }}>
+        <Breadcrumb.Item>Playlists</Breadcrumb.Item>
+        <Breadcrumb.Item>Tracks</Breadcrumb.Item>
+      </Breadcrumb>
       <div className="main-content">
         <Switch>
           <Route exact path="/">
