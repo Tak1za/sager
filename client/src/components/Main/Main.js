e3d0b911635c9f603de133efcb05af10c4716433
Gupta <Varun.Gupta3@honeywell.com>
2020-06-15 13:57:45 +0530

    Fixed a margin issue and moved breadcrumb into a component

--- client/src/components/Main/Main.js
+++ client/src/components/Main/Main.js
@@ -7,16 +7,13 @@ import Playlists from "../Playlists/Playlists";
 import Podcasts from "../Podcasts/Podcasts";
 import Explore from "../Explore/Explore";
 import Login from "../Login/Login";
-import { Breadcrumb } from "react-bootstrap";
+import Crumb from "../Crumb/Crumb";
 
 function Main(props) {
   return (
     <div className="main-container">
       {props.children}
-      <Breadcrumb style={{ backgroundColor: "white" }}>
-        <Breadcrumb.Item>Playlists</Breadcrumb.Item>
-        <Breadcrumb.Item>Tracks</Breadcrumb.Item>
-      </Breadcrumb>
+      <Crumb />
       <div className="main-content">
         <Switch>
           <Route exact path="/">
